bc406f28d494449672c07aad148e0f302c2dcb56
Juan Antonio Xahuentitla Montiel <xahua1@gmail.com>
2021-01-20 21:26:40 -0600

    Agregar archivos de proyecto.

---
+++ HelloWorld/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClickThis = New System.Windows.Forms.Button()
+        Me.lblHelloWorld = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnClickThis
+        '
+        Me.btnClickThis.Location = New System.Drawing.Point(209, 191)
+        Me.btnClickThis.Name = "btnClickThis"
+        Me.btnClickThis.Size = New System.Drawing.Size(75, 23)
+        Me.btnClickThis.TabIndex = 0
+        Me.btnClickThis.Text = "Click this"
+        Me.btnClickThis.UseVisualStyleBackColor = True
+        '
+        'lblHelloWorld
+        '
+        Me.lblHelloWorld.AutoSize = True
+        Me.lblHelloWorld.Location = New System.Drawing.Point(209, 246)
+        Me.lblHelloWorld.Name = "lblHelloWorld"
+        Me.lblHelloWorld.Size = New System.Drawing.Size(41, 15)
+        Me.lblHelloWorld.TabIndex = 1
+        Me.lblHelloWorld.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblHelloWorld)
+        Me.Controls.Add(Me.btnClickThis)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClickThis As Button
+    Friend WithEvents lblHelloWorld As Label
+End Class
